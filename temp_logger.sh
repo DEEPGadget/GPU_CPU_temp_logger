701864a127bd4565a120f0b7f1ff845f7f287659
Jinseo Choi <32115744+JS-Choi513@users.noreply.github.com>
2024-08-07 15:58:16 +0900

    Update temp_logger.sh

--- temp_logger.sh
+++ temp_logger.sh
@@ -15,13 +15,15 @@ INTERVAL=$1
 DURATION=$2
 		    
 # 로그 파일 경로 설정
-LOG_FILE="temperature_log.txt"
+LOG_FILE="thermal_log.csv"
 		    
 # 총 반복 횟수 계산
 REPEAT_COUNT=$((DURATION / INTERVAL))
 
 NUM_GPUS=$(nvidia-smi --query-gpu=count --format=csv,noheader,nounits | sort | uniq)
-HEADER="Timestamp, NVMe_Temperature, CPU0_Tctl, CPU1_Tctl"
+NUM_CPUS=$(sensors | grep -i 'k10temp-pci' | wc -l)
+
+HEADER="Timestamp, NVMe_Temperature"
 for ((j=0; j<NUM_GPUS; j++))
 do
 	HEADER="$HEADER, GPU${j}_Temperature"
@@ -30,6 +32,9 @@ done
 HEADER="$HEADER, MEM_TOTAL_MB, MEM_USED_MB"
 echo $HEADER > $LOG_FILE
 
+
+
+
 # 루프 시작
 for ((i=0; i<REPEAT_COUNT; i++))
 do
@@ -37,14 +42,19 @@ do
 	CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 				        
 	# NVMe 온도 정보 가져오기
-	NVME_TEMP=$(nvme smart-log /dev/nvme0n1 | grep 'temperature' | awk '{print $3"°C"}')
-					        
+	#NVME_TEMP=$(nvme smart-log /dev/nvme0n1 | grep 'temperature' | awk '{print $3"°C"}')
+	NVME_TEMP=$(nvme smart-log /dev/nvme0n1 | grep 'temperature' | awk '{gsub(/[+°C]/, "", $3); print $3}') 
+				        
 	# CPU 온도 정보 가져오기
-	CPU_TEMP0=$(sensors | grep 'Tctl' | awk '/Tctl/ {count++; if (count==1) print $2}')
-	CPU_TEMP1=$(sensors | grep 'Tctl' | awk '/Tctl/ {count++; if (count==2) print $2}')
-					        
+	CPU_TEMPS=""
+	for ((j=0; j<NUM_CPUS; j++))
+	do
+		CPU_TEMP=$(sensors | awk -v cpu=$((j+1)) '/Tctl/ {count++; if (count==cpu) {gsub(/[+°C]/, "", $2); printf("%d", $2 + 0.5); exit}}')
+		# append current CPU_TEMP to previous CPU_TEMPS
+		CPU_TEMPS="$CPU_TEMPS, $CPU_TEMP"
+	done					        
 	# 메모리 온도 정보 가져오기 (예: DDR 메모리 온도)
-	# MEM_TEMP=$(sensors | grep 'temp1' | awk '{print $2}')
+	MEM_TEMP=$(sensors | grep 'temp1' | awk '{print $2}')
 							        
 	# 메모리 사용량 정보 가져오기
 	MEM_TOTAL=$(free -m | awk '/Mem:/ {print $2}')
@@ -54,11 +64,11 @@ do
 	GPU_TEMPS=""
 	for ((j=0; j<NUM_GPUS; j++))
 	do
-		GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits -i $j | awk '{print $1"°C"}')
+		GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits -i $j | awk '{print $1""}')
 		GPU_TEMPS="$GPU_TEMPS, $GPU_TEMP"
 	done	
 	# 로그 파일에 시간과 온도 정보 저장
-	echo "$CURRENT_TIME, $NVME_TEMP, $CPU_TEMP0, $CPU_TEMP1 $GPU_TEMPS, ${MEM_TOTAL}MB, ${MEM_USED}MB" >> $LOG_FILE
+	echo "$CURRENT_TIME, $NVME_TEMP$CPU_TEMPS$GPU_TEMPS, ${MEM_TOTAL}MB, ${MEM_USED}MB" >> $LOG_FILE
 										    
 	# 설정된 간격만큼 대기
 	sleep $INTERVAL
